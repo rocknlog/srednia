f17181a8dd2c6332765d676e19f69fa54a98e1a8
rocknlog <128227228+rocknlog@users.noreply.github.com>
2023-06-06 12:10:32 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        a = inputValue();
+        b = inputValue();
+        c = inputValue();
+        double sredniaArytmetyczna;
+
+        sredniaArytmetyczna = (a + b + c) / 3;
+        Console.WriteLine(sredniaArytmetyczna);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
